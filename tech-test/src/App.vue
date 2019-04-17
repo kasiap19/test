854736f40bb556bc1edb52592b4dd20b7079ffd9
Kasia Pieniazek <kasia@mbp-kasia.mynet>
2019-04-17 22:16:34 +0100

    Displaying countries from JSON, setting up leaflets.js

--- tech-test/src/App.vue
+++ tech-test/src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Feeds />
+    <Map />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
+import Map from './components/Map.vue';
+import Feeds from './components/Feeds.vue';
 
 export default {
   name: 'app',
   components: {
+    Map,
+    Feeds
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/_settings.scss";
+  #app {
+    @include lg {
+      display: grid;
+      grid-template-columns: 150px auto;
+    }
+  }
+
+
 </style>
---
+++ tech-test/src/components/Feeds.vue
@@ -0,0 +1,133 @@
+<template>
+<div class="all">
+    <button class="all__btn" @click="test">display all</button>
+    <div class="all__feeds feeds">
+        <div class="feeds__countries" v-for="item in countries" :key="item.longitude">
+            <button data-dep="xxx">
+                {{item}}
+            </button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Feeds',
+    data() {
+        return {
+            countries: []
+        }
+    },
+
+    created() {
+        // fetch data from JSON and push new locations into locations array so when the app is created 
+        // location with longitudes and latitudes is updated with values and names 
+        fetch("http://s3-eu-west-1.amazonaws.com/omnifi/techtests/locations.json")
+            .then(response => response.json())
+            .then((data) => {
+            this.country = data;
+            
+             for (var i = 0; i < this.country.length; i++) {
+                 this.countries.push(this.country[i].name)
+                // console.log(this.country[i].name)            
+             }
+
+            //  console.log(this.countries);
+        });
+    },
+    methods: {
+        test(event) {
+             event.target.parentNode.classList.toggle('is-active')
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/_settings.scss";
+
+.all {
+
+    &__btn {
+        &:after {
+            content: '\f107';
+            font-family: "Font Awesome 5 Free";
+            font-size: 15px;
+            margin-left: 10px;
+            transition: .3s;
+        }
+
+        text-transform: uppercase;
+        width: 100%;
+        height: 40px;
+        font-weight: bold;
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;    
+
+        @include lg {
+            display: none;
+        }
+    }
+
+    &__feeds {
+        display: none;
+
+        @include lg {
+            display: flex;
+            flex-direction: column;
+            background: #ddd;
+        }
+    }
+
+    &.is-active {
+        .all {
+            &__btn {
+                background: #e5e6e8;
+
+                &:after {
+                    transform: rotate(180deg);
+                }
+            }
+
+            &__feeds {
+                display: flex;
+                flex-direction: column;
+
+                @include md {
+                    display: grid;
+                    grid-template-columns: repeat(3, 1fr);
+                }
+
+                button {
+                    padding: 10px 40px;
+                }
+            }
+        }
+    }
+}
+    .feeds {
+        @include lg {
+            height: 100vh;
+            overflow: scroll;
+        }
+
+        &__countries {
+            width: 100%;
+        }
+
+        button {
+            font-weight: bold;
+            padding: 10px;
+            line-height: 18px;
+            width: 100%;
+
+            @include lg {
+                padding: 20px;
+            }
+        
+        }
+    }
+</style>
